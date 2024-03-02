e5560ff20ac570eef4985b2bc109cb229d3c08f7
Muhammad3347 <126417768+Muhammad3347@users.noreply.github.com>
2024-03-02 19:38:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var imageUrl = "https://w0.peakpx.com/wallpaper/925/950/HD-wallpaper-green-hacker-banned-fail-greenhacker-hacked-hayatikdrgl-screenhacked.jpg";
+
+    var fullScreenImage = document.getElementById("fullScreenImage");
+    fullScreenImage.style.backgroundImage = "url('" + imageUrl + "')";
+});
